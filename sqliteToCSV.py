e6af252c0c9ef0dc78722a483fd7eb6b023e1a7c
kc <kc@KCEKALI.lab>
2018-12-27 09:13:04 +0100

    update sqliteToCSV to use pandas

--- sqliteToCSV.py
+++ sqliteToCSV.py
@@ -1,27 +1,28 @@
-import sqlite3, csv
+import dataset, pandas as pd
 
+db = dataset.connect("sqlite:///.vscode/streamSentimentAnalysis.db")
+
+outputDict = {}
+for tweet in db['tweets']:
+    outputDict[tweet['id']] = [tweet['id_str'], tweet['user_name'], tweet['created'], tweet['text'], tweet['polarity'], tweet['subjectivity']]
+
+pd.DataFrame.from_dict(outputDict, orient='index', columns=['id_str', 'user_name', 'created', 'text', 'polarity', 'subjectivity']).to_csv('.vscode/output.csv')
+
+""" 
 conn = sqlite3.connect('.vscode/streamSentimentAnalysis.db')
 cur = conn.cursor()
 
-data = cur.execute('SELECT * from tweets')
+data = cur.execute('SELECT id, user_name, text, polarity, subjectivity from tweets')
 # columns = cur.execute('PRAGMA table_info(tweets)')
 
 with open('.vscode/output.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow([
-        'user_description',
-        'user_location',
-        'coordinates',
-        'text',
+        'id',
         'user_name',
-        'user_created',
-        'user_followers',
-        'id_str',
-        'created',
-        'retweet_count',
-        'user_bg_color',
+        'text',
         'polarity',
         'subjectivity',
         ])
     writer.writerows(data)
-conn.close()
+conn.close() """
